0c830fbaf2fbc6914e1193a5ed24fbdcf56851a0
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2024-10-10 22:56:08 -0300

    style: layout colors

--- app/page.tsx
+++ app/page.tsx
@@ -23,22 +23,13 @@ export default function Home() {
   const progress = (checkedCount / totalItems) * 100;
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="space-y-4 py-12">
-        <div className="sticky bottom-4 w-full ">
-          <div className="h-4 bg-gray-300 rounded-full">
-            <div
-              className={`h-full bg-indigo-500 rounded-full w-[${progress}%]`}
-              style={{ width: `${progress}%` }}
-            />
-          </div>
-          <p className="text-center mt-2">{Math.round(progress)}% concluído</p>
-        </div>
+    <div className="flex flex-col justify-center items-center bg-gray-300 dark:bg-zinc-900 py-12">
+      <div className="space-y-8 p-4 border border-violet-400 rounded-xl">
         {checklistItems.map((group, index) => (
           <div key={index} className="space-y-2">
             <h2 className="font-semibold text-2xl">{group.category}</h2>
             {group.items.map((item) => (
-              <div key={item.text} className="flex flex-col space-y-2">
+              <div key={item.text} className="flex flex-col space-y-1">
                 <label
                   htmlFor={item.text}
                   className="flex cursor-pointer items-center"
@@ -47,7 +38,7 @@ export default function Home() {
                     type="checkbox"
                     id={item.text}
                     value=""
-                    className="size-4 appearance-none border cursor-pointer border-gray-300  rounded-md mr-2 hover:border-indigo-500 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100"
+                    className="size-4 appearance-none border cursor-pointer border-gray-300 rounded-md mr-2 hover:border-violet-500 checked:bg-no-repeat checked:bg-center checked:border-violet-500 checked:bg-violet-100"
                     onChange={() => handleCheckboxChange(item.text)}
                   />
                   {item.text}
@@ -59,6 +50,17 @@ export default function Home() {
             ))}
           </div>
         ))}
+        <div className="sticky bottom-4 w-full bg-zinc-950/90 p-2 backdrop-blur-sm rounded-2xl">
+          <div className="h-5 bg-violet-300 rounded-full relative">
+            <div
+              className="h-full bg-purple-500 rounded-full"
+              style={{ width: `${progress}%` }}
+            />
+            <p className="absolute inset-0 flex items-center justify-center text-zinc-900 text-sm font-semibold">
+              {Math.round(progress)}% concluído
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
